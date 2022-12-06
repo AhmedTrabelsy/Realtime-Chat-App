758fd168671905dc42d59e734504b8e8c34fc1cf
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-07 00:49:36 +0100

    Making responsive containers chat , friends.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app">
+  <div id="app" :style="{ backgroundImage: 'url(' + background + ')' }">
+    <div class="d-flex justify-content-between">
+      <div class="managing"></div>
+      <div class="chat"></div>
+    </div>
     <login-page />
   </div>
 </template>
@@ -12,6 +16,54 @@ export default {
   components: {
     loginPage,
   },
+  data() {
+    return {
+      background: require("../src/assets/background.jpg"),
+    };
+  },
 };
 document.title = "TuniChat";
 </script>
+
+<style scoped>
+/* html,
+body {
+  margin: 0;
+  height: 100%;
+  overflow: hidden;
+} */
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  width: 100vw;
+  height: 100vh;
+  padding: 3vh 3vw 0 3vw;
+}
+
+.managing {
+  width: 30vw;
+  height: 93vh;
+  background-color: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(30px);
+  border: 2px solid transparent;
+  border-radius: 2%;
+  background-clip: padding-box;
+}
+
+.chat {
+  width: 63vw;
+  height: 93vh;
+  background-color: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(30px);
+  border: 2px solid transparent;
+  border-radius: 2%;
+  background-clip: padding-box;
+}
+</style>
