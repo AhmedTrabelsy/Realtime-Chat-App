0d07c17a08c4f1158b9d492eb056faeab8e20b15
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-06 23:18:35 +0100

    Creating website title and icon

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <login-page />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import loginPage from "@/components/login-page.vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    loginPage,
+  },
+};
+document.title = "TuniChat";
+</script>
