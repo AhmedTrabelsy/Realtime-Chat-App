5bf17a91df84f6d5c156ddaa72ac03d71727c2c1
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-08 21:45:00 +0100

    creating media queryies for mobile,tab,desk.

--- app/src/components/login-page.vue
+++ app/src/components/login-page.vue
@@ -1,28 +1,43 @@
 <template>
-  <div class="container d-flex justify-content-center">
-    <div class="login p-5">
+  <div class="d-flex justify-content-center">
+    <div class="login">
       <div class="login-logo container">
         <Vue3Lottie :animationData="login" />
       </div>
-      <div>
-        <h1>Login</h1>
-        <p>Welcome back please login to your account</p>
+      <div class="login-form-container">
+        <div class="container mb-3">
+          <h1 class="display-3">Login</h1>
+          <p>Welcome back please login to your account</p>
+        </div>
+        <form class="container d-flex flex-column">
+          <div class="form-group mb-3">
+            <label for="email">Email address</label>
+            <input
+              type="email"
+              class="form-control bg-transparent mt-2"
+              id="email"
+            />
+          </div>
+          <div class="form-group mb-1">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              class="form-control bg-transparent mt-2"
+              id="password"
+            />
+          </div>
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="remember" />
+            <label class="form-check-label" for="remember">Remember me</label>
+          </div>
+          <button type="submit" class="btn btn-outline-danger submit-login">
+            Login
+          </button>
+        </form>
+        <div class="container text-center mt-4">
+          <p>Don't Have an account ? <a href="">SignUp</a></p>
+        </div>
       </div>
-      <form class="container">
-        <div class="form-group">
-          <label for="email">Email address</label>
-          <input type="email" class="form-control" id="email" />
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" />
-        </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="remember" />
-          <label class="form-check-label" for="remember">Remember me</label>
-        </div>
-        <button type="submit" class="btn btn-outline-light">LogIn</button>
-      </form>
     </div>
   </div>
 </template>
@@ -44,51 +59,89 @@ export default {
 
 <style>
 .login {
-  width: 40vw;
-  height: 94vh;
   background-color: rgba(251, 251, 251, 0.25);
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(10px);
   border: 1px solid rgba(255, 255, 255, 0.18);
   border-radius: 30px;
   background-clip: padding-box;
+  padding: 5vw;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
-.login-logo {
-  width: 200px;
+input {
+  background: transparent;
+  border: none;
 }
 
-@media only screen and (max-width: 1200px) {
-  /*Tablets [601px -> 1200px]*/
+h1,
+label,
+p {
+  color: #eee;
+}
+
+a {
+  color: yellow;
+}
+
+/* Small devices (landscape phones, 576px and up) */
+@media (max-width: 480px) {
   .login {
     width: 90vw;
     height: 85vh;
   }
+  .login-logo {
+    width: 200px;
+  }
+
+  .submit-login {
+    margin-top: 30px;
+  }
+}
+/* Medium devices (tablets, 768px and up) The navbar toggle appears at this breakpoint */
+@media (min-width: 481px) {
+  .login {
+    width: 70vw;
+    height: 85vh;
+    padding: 0 2vw;
+  }
+  .login-logo {
+    width: 200px;
+  }
+
+  .login-form-container {
+    padding: 0 100px;
+  }
+
+  .submit-login {
+    margin-top: 30px;
+  }
+}
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 1100px) {
+  .login {
+    width: 40vw;
+    height: 85vh;
+    padding: 2vw;
+  }
+
   .login-logo {
     width: 300px;
   }
-}
 
-@media only screen and (max-width: 600px) {
-  /*Big smartphones [426px -> 600px]*/
-  .login {
-    width: 90vw;
-    height: 85vh;
-  }
-  .login-logo {
-    width: 100px;
-  }
-}
-
-@media only screen and (max-width: 425px) {
-  /*Small smartphones [325px -> 425px]*/
-  .login {
-    width: 90vw;
-    height: 85vh;
+  .login-form-container {
+    padding: 0 100px;
   }
 
-  .login-logo {
-    width: 200px;
+  * {
+    font-size: large;
+  }
+
+  .submit-login {
+    margin-top: 50px;
   }
 }
 </style>
