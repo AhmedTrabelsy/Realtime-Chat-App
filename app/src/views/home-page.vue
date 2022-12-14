8b190a2b1b7233d855c14262249994c50321cab4
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-14 22:27:32 +0100

    home page responsive

--- app/src/views/home-page.vue
+++ app/src/views/home-page.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="home-container container d-grid centred-container">
-    <div class="row py-5">
-      <div class="col-sm-5 my-auto">
-        <lottieVue animation="home" />
+  <div class="home-container container d-flex justify-content-between centred-container">
+    <div class="my-auto animation">
+      <lottieVue animation="home" />
+    </div>
+    <div class="register glass-container px-5 my-5">
+      <img :src="logoUrl" alt="home logo" class="logo mx-auto d-block my-3" />
+      <h1 class="display-5">Join us now !</h1>
+      <hr />
+      <p class="display-6 fs-5">Connect with friends and the world around you on <strong>TuniChat</strong>.</p>
+      <div class="px-5 pt-3 quote">
+        <p class="fs-6">{{ quote }}</p>
+        <p class="d-flex justify-content-end">
+          <i>-- {{ author }} --</i>
+        </p>
       </div>
-      <div class="register col-sm-7 glass-container px-5">
-        <img :src="logoUrl" alt="home logo" class="logo mx-auto d-block my-3" />
-        <h1 class="display-5">Join us now !</h1>
-        <hr />
-        <p class="display-6 fs-5">Connect with friends and the world around you on <strong>TuniChat</strong>.</p>
-        <div class="px-5 pt-3 quote">
-          <p class="fs-6">{{ quote }}</p>
-          <p class="d-flex justify-content-end">
-            <i>-- {{ author }} --</i>
-          </p>
-        </div>
-        <div class="buttons btn-group d-flex justify-content-center bluryButton">
-          <router-link :to="{ name: 'login' }" class="btn btn-outline-light">Login</router-link>
-          <router-link :to="{ name: 'signup' }" class="btn btn-outline-light">SignUp</router-link>
-        </div>
+      <div class="buttons btn-group d-flex justify-content-center bluryButton">
+        <router-link :to="{ name: 'login' }" class="btn btn-outline-light">Login</router-link>
+        <router-link :to="{ name: 'signup' }" class="btn btn-outline-light">SignUp</router-link>
       </div>
     </div>
   </div>
@@ -67,7 +65,7 @@ export default {
   width: 100vw;
   height: 100vh;
   overflow-x: hidden;
-  overflow-y: auto;
+  overflow-y: hidden;
 }
 
 .logo {
@@ -81,5 +79,24 @@ export default {
   min-height: 30vh;
   max-height: 30vh;
   overflow: inherit;
+}
+.animation {
+  width: 30vw;
+}
+@media (max-width: 480px) {
+  .animation {
+    display: none;
+  }
+  .logo {
+    width: 16rem;
+  }
+}
+@media (max-width: 1000px) {
+  .animation {
+    display: none;
+  }
+  .logo {
+    width: 16rem;
+  }
 }
 </style>
