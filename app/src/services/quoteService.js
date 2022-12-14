927be2d32c7c6221b5fd1558996220e79da8b7cc
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-14 17:24:17 +0100

    Add quote fonctionality

--- app/src/services/quoteService.js
+++ app/src/services/quoteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const quotes = axios.create({
+const options = {
   method: "GET",
   url: "https://famous-quotes4.p.rapidapi.com/random",
   params: { category: "all", count: "2" },
@@ -8,10 +8,10 @@ const quotes = axios.create({
     "X-RapidAPI-Key": "c4915f615emshd6f4761193b6f3dp17bd68jsnbbcb2b9c38bc",
     "X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com",
   },
-});
+};
 
 export default {
   getQuote() {
-    return quotes.get;
+    return axios.request(options);
   },
 };
--- app/src/views/home-page.vue
+++ app/src/views/home-page.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home-container container d-grid centred-container">
     <div class="row py-5">
-      <div class="col-sm-5 my-auto"><lottieVue animation="home" /></div>
+      <div class="col-sm-5 my-auto">
+        <lottieVue animation="home" />
+      </div>
       <div class="register col-sm-7 glass-container px-5">
         <img :src="logoUrl" alt="home logo" class="logo mx-auto d-block my-3" />
         <h1 class="display-5">Join us now !</h1>
         <hr />
         <p class="display-6 fs-5">Connect with friends and the world around you on <strong>TuniChat</strong>.</p>
+        <div class="px-5 pt-3 quote">
+          <p class="fs-6">{{ quote }}</p>
+          <p class="d-flex justify-content-end">
+            <i>-- {{ author }} --</i>
+          </p>
+        </div>
         <div class="buttons btn-group d-flex justify-content-center bluryButton">
           <router-link :to="{ name: 'login' }" class="btn btn-outline-light">Login</router-link>
           <router-link :to="{ name: 'signup' }" class="btn btn-outline-light">SignUp</router-link>
@@ -18,16 +26,38 @@
 
 <script>
 import lottieVue from "@/components/lottie.vue";
+import quoteService from "@/services/quoteService.js";
 
 export default {
   name: "homePage",
   components: {
     lottieVue,
   },
+  data() {
+    return {
+      quotes: {},
+    };
+  },
   computed: {
     logoUrl() {
       return require("@/assets/logo.png");
     },
+    quote() {
+      return this.quotes.text;
+    },
+    author() {
+      return this.quotes.author;
+    },
+  },
+  created() {
+    quoteService
+      .getQuote()
+      .then((response) => {
+        this.quotes = response.data[0];
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   },
 };
 </script>
@@ -39,13 +69,17 @@ export default {
   overflow-x: hidden;
   overflow-y: auto;
 }
+
 .logo {
   width: 25rem;
 }
+
 .register strong {
   color: #6d0073;
 }
-.buttons {
-  margin-top: 23vh;
+.quote {
+  min-height: 30vh;
+  max-height: 30vh;
+  overflow: inherit;
 }
 </style>
