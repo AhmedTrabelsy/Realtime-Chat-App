953e43706bfba99716226475c18bb255f44fffd1
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2022-12-14 16:13:53 +0100

    Create quoteService.js

---
+++ app/src/services/quoteService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const quotes = axios.create({
+  method: "GET",
+  url: "https://famous-quotes4.p.rapidapi.com/random",
+  params: { category: "all", count: "2" },
+  headers: {
+    "X-RapidAPI-Key": "c4915f615emshd6f4761193b6f3dp17bd68jsnbbcb2b9c38bc",
+    "X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com",
+  },
+});
+
+export default {
+  getQuote() {
+    return quotes.get;
+  },
+};
