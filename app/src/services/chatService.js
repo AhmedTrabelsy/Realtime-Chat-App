b94d1cb7ea3650abe44e0746fa4f1a5c3d8c5b21
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2023-01-13 13:57:49 +0100

    test

--- app/src/services/chatService.js
+++ app/src/services/chatService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost/Api/",
+  baseURL: "lkqsddf",
   withCredentials: false,
   headers: {
     Accept: "application/json",
