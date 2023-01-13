e75f0e1dd43c0c91e58f0d1f6295e998e2eafbcb
Ahmed-Trabelsi0 <trabelsig37@gmail.com>
2023-01-13 13:58:21 +0100

    Revert "test"
    This reverts commit b94d1cb7ea3650abe44e0746fa4f1a5c3d8c5b21.

--- app/src/services/chatService.js
+++ app/src/services/chatService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "lkqsddf",
+  baseURL: "http://localhost/Api/",
   withCredentials: false,
   headers: {
     Accept: "application/json",
